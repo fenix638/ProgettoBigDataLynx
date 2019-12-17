b47baebda353fe0c273d5c1e49f97c9a26303476
Scott Thushyanthan <scott.thushyanthan@lynxspa.com>
2019-12-17 12:30:36 +0100

    setup json parse

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,4 @@
+import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 object Main {
@@ -5,6 +6,12 @@ object Main {
 
     val conf = new SparkConf().setMaster("local[2]").setAppName("CountingSheep")
     val sc = new SparkContext(conf);
+
+    val sqlContext = new HiveContext(sc)
+
+    val jsonDF = sqlContext.read.json("C:\\Users\\Studente\\Downloads\\2018-03-01-0.json")
+
+    println(jsonDF.toString())
 
   }
 }
