5f8dafbb8574e6c2128883dc7fb635beceaf4ea2
Scott Thushyanthan <scott.thushyanthan@lynxspa.com>
2019-12-17 11:57:33 +0100

    added sparkCOnf and SparkContext

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,10 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
 object Main {
   def main(args: Array[String]): Unit ={
-    println("CIOA");
+
+    val conf = new SparkConf().setMaster("local[2]").setAppName("CountingSheep")
+    val sc = new SparkContext(conf);
 
   }
 }
