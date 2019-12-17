79aadcf3befd758900e594c86fd1632a9b46bda1
lscutillo <lscutillo@gmail.com>
2019-12-17 17:45:25 +0100

    Commit scu case class for merge master

---
+++ src/main/scala/PullRequest.scala
@@ -0,0 +1,5 @@
+case class PullRequest(diff_url : String,
+                       html_url : String,
+                       patch_url : String,
+                       url : String
+                      )
---
+++ src/main/scala/commitsAttribute.scala
@@ -0,0 +1,6 @@
+case class commitsAttribute(sha : String,
+                            author : Author,
+                            message : String,
+                            distinct : Boolean,
+                            url : String
+                            )
