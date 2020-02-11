6d3b4102c32f1e1a82a60301b7c4a651d94cb410
Scott Thushyanthan <scott.thushyanthan@lynxspa.com>
2020-02-11 10:24:23 +0100

    Actor of THe final Object

---
+++ src/main/scala/ActorFinale.scala
@@ -0,0 +1,8 @@
+case class ActorFinale(
+                      id:Long,
+                      login:String,
+                      display_login:String,
+                      gravatar_id:String,
+                      url:String,
+                      avatar_url:String
+                      )
